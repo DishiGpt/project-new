de26f1e8a618276409d843bf302044877612bcb8
DishiGpt <dishigpt15@gmail.com>
2025-11-05 02:22:35 +0530

    some changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -7,3 +7,4 @@ export const JOB_API_END_POINT = import.meta.env.VITE_BACKEND_URL
     : "https://ignitex-e9n0.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT = `${API_BASE_URL}/api/v1/application`;
 export const COMPANY_API_END_POINT = `${API_BASE_URL}/api/v1/company`;
+//adding 
